6c62c874bae5507c385d4d32aa003e1df11eb196
surprisedPikachu69 <arvind7447@gmail.com>
2023-01-30 11:49:16 +0530

    divide by zero

---
+++ basics/DivideByZero.java
@@ -0,0 +1,12 @@
+package basics;
+class DivideByZero {
+    public static void main(String args[]) {
+        /*
+        int a = 1/0; // ArithmeticException: / by zero
+        int b = 0/0; // ArithmeticException: / by zero
+        double c = 1.0/0; // Infinity
+        double d = -1.0/0; // -Infinity
+        double e = 0.0/0; // NaN
+        */
+    }
+}
