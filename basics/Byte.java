4b7fd9a1e3f2c3c0f4605bd0554f1d826cea94e0
surprisedPikachu69 <arvind7447@gmail.com>
2023-01-30 11:48:12 +0530

    byte

---
+++ basics/Byte.java
@@ -0,0 +1,9 @@
+package basics;
+class Byte {
+    public static void main(String args[]) {
+        byte a = 127;
+        byte b = 20;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
